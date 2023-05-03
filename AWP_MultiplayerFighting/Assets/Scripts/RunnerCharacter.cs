149120b4947471ed178e30800d0914ade6e9e2f5
Benjamin Low <benjaminlow121@gmail.com>
2023-05-03 11:41:48 +0800

    Runner speeds up over time

--- AWP_MultiplayerFighting/Assets/Scripts/RunnerCharacter.cs
+++ AWP_MultiplayerFighting/Assets/Scripts/RunnerCharacter.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RunnerCharacter : MonoBehaviour
 {
 
-    public float speed = 2;
+    float speed;
+    public float jumpHeight = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class RunnerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float speed = Time.time;
         Vector3 movement = new Vector3 (1, 0, 0);
         transform.Translate(movement * speed * Time.deltaTime);
     }
