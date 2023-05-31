0e22a94920e616b7a1f22c74ae36c5453bbd5bef
Benjamin Low <benjaminlow121@gmail.com>
2023-05-31 12:33:57 +0800

    Updated Line, Can work on Wacom

--- AWP_EndlessRunner/Assets/Scripts/Endless Runner/RunnerCharacter.cs
+++ AWP_EndlessRunner/Assets/Scripts/Endless Runner/RunnerCharacter.cs
@@ -7,19 +7,29 @@ public class RunnerCharacter : MonoBehaviour
 
     public GameObject runner;
     float speed;
+    float mode;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mode = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        if (mode == 1){
+        float speed = Time.time;
+        Vector3 movement = new Vector3 (1, 0, 0);
+        transform.Translate(movement * speed * 1);
+        }
+
+        if (mode == 2){
         float speed = Time.time;
         Vector3 movement = new Vector3 (1, 0, 0);
         transform.Translate(movement * speed * Time.deltaTime);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space)){
             Debug.Log(speed);
@@ -33,5 +43,14 @@ public class RunnerCharacter : MonoBehaviour
             Destroy(runner);
         }
     }
+
+    void SwitchToHard(){
+        mode = 2;
+    }
+
+    void SwitchToEasy(){
+        mode = 1;
+    }
+
 
 }
